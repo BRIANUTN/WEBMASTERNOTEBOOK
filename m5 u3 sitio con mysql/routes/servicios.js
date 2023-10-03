905b7cf925b0d08f26dffce9ee30a6353f3451d5
BRIANUTN <132473128+BRIANUTN@users.noreply.github.com>
2023-10-03 12:19:52 -0300

    servicios hbs

--- m5 u3 sitio con mysql/routes/servicios.js
+++ m5 u3 sitio con mysql/routes/servicios.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var novedadesModel = require('../models/novedadesModels');
 
 router.get('/', async function(req, res, next) {
 
+  var novedades = await novedadesModel.getNovedades();
   
   res.render('servicios',{
-    isServicios:true
+    isServicios:true,
+    novedades
   });
 });
 
